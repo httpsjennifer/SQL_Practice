cd59359853216048fb03bf707abd68776c708ef7
Jens <jenniferk973@outlook.com>
2026-08-09 12:30:41 +0100

    Add table for Quarter Shank Burger ingredients

--- Dollys_Parlour.sql
+++ Dollys_Parlour.sql
@@ -30,3 +30,32 @@ added_item_1 TEXT,
 added_item_2 TEXT,
 added_item_3 TEXT
 );
+
+-- (2) The Quarter Shank Burger: Bacon Cheese-Button Burger Ingredients
+-- True means keep og ingredient, false + answer means swapped, false + none means removed
+CREATE TABLE  the_quarter_shank_burger   (
+qs_burger_id INTEGER, 
+-- (2.1) Tracking the bun | what is kept swapped or removed
+original_bun BOOLEAN,
+chosen_bun TEXT,
+ -- (2.2) Tracking the chosen protein | what is kept swapped or removed
+beef BOOLEAN, 
+chosen_protein TEXT,
+-- (2.3) Tracking the vegetables and bacon | what is kept or removed
+lettuce BOOLEAN,
+tomato BOOLEAN,
+pickles BOOLEAN,
+bacon BOOLEAN,
+-- (2.4) Tracking the cheese | kept, swapped or removed
+cheese BOOLEAN,
+chosen_cheese TEXT,
+-- (2.5) Tracking the sauces | is it kept swapped or removed
+burger_sauce BOOLEAN,
+tomato_sauce BOOLEAN,
+chosen_sauce_1 TEXT,
+chosen_sauce_2 TEXT,
+-- (2.6) Tracking the added items 
+added_item_1 TEXT,
+added_item_2 TEXT,
+added_item_3 TEXT
+);
