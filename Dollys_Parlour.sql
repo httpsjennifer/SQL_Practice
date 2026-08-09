8fc4361770ef0247c24d06c5c6d248ca0eb10c71
Jens <jenniferk973@outlook.com>
2026-08-09 14:59:25 +0100

    Enforce not null constraints and add primary key
    Updated the_full_shank_burger table to enforce not null constraints on several columns and added primary key.

--- Dollys_Parlour.sql
+++ Dollys_Parlour.sql
@@ -120,32 +120,35 @@ added_item_3 TEXT
 -- (5) The Full Shank Burger: Hashbrown Beef and Chicken Button Burger Ingredients
 -- True means keep og ingredient, false + answer means swapped, false + none means removed
 CREATE TABLE the_full_shank_burger  (
-qs_burger_id INTEGER, 
+customer_id INTEGER not null,
+qs_burger_id INTEGER not null, 
 -- (5.1) Tracking the bun | what is kept swapped or removed
-original_bun BOOLEAN,
-chosen_bun TEXT,
+original_bun BOOLEAN not null,
+chosen_bun TEXT null,
  -- (5.2) Tracking the chosen protein | what is kept swapped or removed
-beef BOOLEAN, 
-chosen_protein TEXT,
-chicken BOOLEAN,
-chosen_protein_2 TEXT,
+beef BOOLEAN not null, 
+chosen_protein TEXT not null,
+chicken BOOLEAN not null,
+chosen_protein_2 TEXT null,
 -- (5.3) Tracking the hashbrown | kept or removed
 hashbrown BOOLEAN,
 -- (5.4) Tracking the vegetables and bacon | what is kept or removed
-lettuce BOOLEAN,
-tomato BOOLEAN,
-pickles BOOLEAN,
-bacon BOOLEAN,
+lettuce BOOLEAN not null,
+tomato BOOLEAN not null,
+pickles BOOLEAN not null,
+bacon BOOLEAN not null,
 -- (5.5) Tracking the cheese | kept, swapped or removed
-cheese BOOLEAN,
-chosen_cheese TEXT,
+cheese BOOLEAN not null,
+chosen_cheese TEXT null,
 -- (5.6) Tracking the sauces | is it kept swapped or removed
-burger_sauce BOOLEAN,
-chosen_sauce_1 TEXT,
+burger_sauce BOOLEAN not null,
+chosen_sauce_1 TEXT null,
 -- (5.7) Tracking the added items 
-added_item_1 TEXT,
-added_item_2 TEXT,
-added_item_3 TEXT
+added_item_1 TEXT null,
+added_item_2 TEXT null,
+added_item_3 TEXT null,
+primary key (qs_burger_id)
+);
 
 -- (6) The Abalone Burger: Fish Fillet Button Burger Ingredients
 -- True means keep og ingredient, false + answer means swapped, false + none means removed
