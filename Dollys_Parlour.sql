c2bceb3ff00140bd037622b74d4fb2383e0609ec
Jens <jenniferk973@outlook.com>
2026-08-09 15:33:12 +0100

    Change ideal_stock_amount type from TEXT to INTEGER

--- Dollys_Parlour.sql
+++ Dollys_Parlour.sql
@@ -236,7 +236,7 @@ ingredients_id INTEGER not null,
 ingredient TEXT not null,
 quantity INTEGER not null,
 measurement TEXT not null,
-ideal_stock_amount TEXT not null,
+ideal_stock_amount INTEGER not null,
 stock_left INTEGER not null
 primary key (ingredients_id)
 );
